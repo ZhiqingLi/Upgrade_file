34d40b9eb04911e5ebd0dfeb89f7684a75251355
ZhiqingLi <294518096@163.com>
2019-08-23 21:07:08 +0800

    refresh 190823

--- Bluetrum/D85/res.h
+++ Bluetrum/D85/res.h
@@ -47,106 +47,109 @@
 #define RES_BUF_EN_DISCON_HID_MP3                   (*(u32 *)0x110001f8)
 #define RES_LEN_EN_DISCON_HID_MP3                   (*(u32 *)0x110001fc)
 
-#define RES_BUF_EN_FM_MODE_MP3                      (*(u32 *)0x11000218)
-#define RES_LEN_EN_FM_MODE_MP3                      (*(u32 *)0x1100021c)
+#define RES_BUF_EN_FACTORY_MP3                      (*(u32 *)0x11000218)
+#define RES_LEN_EN_FACTORY_MP3                      (*(u32 *)0x1100021c)
 
-#define RES_BUF_EN_LANGUAGE_MP3                     (*(u32 *)0x11000238)
-#define RES_LEN_EN_LANGUAGE_MP3                     (*(u32 *)0x1100023c)
+#define RES_BUF_EN_FM_MODE_MP3                      (*(u32 *)0x11000238)
+#define RES_LEN_EN_FM_MODE_MP3                      (*(u32 *)0x1100023c)
 
-#define RES_BUF_EN_LEFT_CH_MP3                      (*(u32 *)0x11000258)
-#define RES_LEN_EN_LEFT_CH_MP3                      (*(u32 *)0x1100025c)
+#define RES_BUF_EN_LANGUAGE_MP3                     (*(u32 *)0x11000258)
+#define RES_LEN_EN_LANGUAGE_MP3                     (*(u32 *)0x1100025c)
 
-#define RES_BUF_EN_LOW_BATTERY_MP3                  (*(u32 *)0x11000278)
-#define RES_LEN_EN_LOW_BATTERY_MP3                  (*(u32 *)0x1100027c)
+#define RES_BUF_EN_LEFT_CH_MP3                      (*(u32 *)0x11000278)
+#define RES_LEN_EN_LEFT_CH_MP3                      (*(u32 *)0x1100027c)
 
-#define RES_BUF_EN_MAX_VOL_MP3                      (*(u32 *)0x11000298)
-#define RES_LEN_EN_MAX_VOL_MP3                      (*(u32 *)0x1100029c)
+#define RES_BUF_EN_LOW_BATTERY_MP3                  (*(u32 *)0x11000298)
+#define RES_LEN_EN_LOW_BATTERY_MP3                  (*(u32 *)0x1100029c)
 
-#define RES_BUF_EN_MUSIC_MODE_MP3                   (*(u32 *)0x110002b8)
-#define RES_LEN_EN_MUSIC_MODE_MP3                   (*(u32 *)0x110002bc)
+#define RES_BUF_EN_MAX_VOL_MP3                      (*(u32 *)0x110002b8)
+#define RES_LEN_EN_MAX_VOL_MP3                      (*(u32 *)0x110002bc)
 
-#define RES_BUF_EN_NUM_0_MP3                        (*(u32 *)0x110002d8)
-#define RES_LEN_EN_NUM_0_MP3                        (*(u32 *)0x110002dc)
+#define RES_BUF_EN_MUSIC_MODE_MP3                   (*(u32 *)0x110002d8)
+#define RES_LEN_EN_MUSIC_MODE_MP3                   (*(u32 *)0x110002dc)
 
-#define RES_BUF_EN_NUM_1_MP3                        (*(u32 *)0x110002f8)
-#define RES_LEN_EN_NUM_1_MP3                        (*(u32 *)0x110002fc)
+#define RES_BUF_EN_NUM_0_MP3                        (*(u32 *)0x110002f8)
+#define RES_LEN_EN_NUM_0_MP3                        (*(u32 *)0x110002fc)
 
-#define RES_BUF_EN_NUM_2_MP3                        (*(u32 *)0x11000318)
-#define RES_LEN_EN_NUM_2_MP3                        (*(u32 *)0x1100031c)
+#define RES_BUF_EN_NUM_1_MP3                        (*(u32 *)0x11000318)
+#define RES_LEN_EN_NUM_1_MP3                        (*(u32 *)0x1100031c)
 
-#define RES_BUF_EN_NUM_3_MP3                        (*(u32 *)0x11000338)
-#define RES_LEN_EN_NUM_3_MP3                        (*(u32 *)0x1100033c)
+#define RES_BUF_EN_NUM_2_MP3                        (*(u32 *)0x11000338)
+#define RES_LEN_EN_NUM_2_MP3                        (*(u32 *)0x1100033c)
 
-#define RES_BUF_EN_NUM_4_MP3                        (*(u32 *)0x11000358)
-#define RES_LEN_EN_NUM_4_MP3                        (*(u32 *)0x1100035c)
+#define RES_BUF_EN_NUM_3_MP3                        (*(u32 *)0x11000358)
+#define RES_LEN_EN_NUM_3_MP3                        (*(u32 *)0x1100035c)
 
-#define RES_BUF_EN_NUM_5_MP3                        (*(u32 *)0x11000378)
-#define RES_LEN_EN_NUM_5_MP3                        (*(u32 *)0x1100037c)
+#define RES_BUF_EN_NUM_4_MP3                        (*(u32 *)0x11000378)
+#define RES_LEN_EN_NUM_4_MP3                        (*(u32 *)0x1100037c)
 
-#define RES_BUF_EN_NUM_6_MP3                        (*(u32 *)0x11000398)
-#define RES_LEN_EN_NUM_6_MP3                        (*(u32 *)0x1100039c)
+#define RES_BUF_EN_NUM_5_MP3                        (*(u32 *)0x11000398)
+#define RES_LEN_EN_NUM_5_MP3                        (*(u32 *)0x1100039c)
 
-#define RES_BUF_EN_NUM_7_MP3                        (*(u32 *)0x110003b8)
-#define RES_LEN_EN_NUM_7_MP3                        (*(u32 *)0x110003bc)
+#define RES_BUF_EN_NUM_6_MP3                        (*(u32 *)0x110003b8)
+#define RES_LEN_EN_NUM_6_MP3                        (*(u32 *)0x110003bc)
 
-#define RES_BUF_EN_NUM_8_MP3                        (*(u32 *)0x110003d8)
-#define RES_LEN_EN_NUM_8_MP3                        (*(u32 *)0x110003dc)
+#define RES_BUF_EN_NUM_7_MP3                        (*(u32 *)0x110003d8)
+#define RES_LEN_EN_NUM_7_MP3                        (*(u32 *)0x110003dc)
 
-#define RES_BUF_EN_NUM_9_MP3                        (*(u32 *)0x110003f8)
-#define RES_LEN_EN_NUM_9_MP3                        (*(u32 *)0x110003fc)
+#define RES_BUF_EN_NUM_8_MP3                        (*(u32 *)0x110003f8)
+#define RES_LEN_EN_NUM_8_MP3                        (*(u32 *)0x110003fc)
 
-#define RES_BUF_EN_PAIRING_MP3                      (*(u32 *)0x11000418)
-#define RES_LEN_EN_PAIRING_MP3                      (*(u32 *)0x1100041c)
+#define RES_BUF_EN_NUM_9_MP3                        (*(u32 *)0x11000418)
+#define RES_LEN_EN_NUM_9_MP3                        (*(u32 *)0x1100041c)
 
-#define RES_BUF_EN_PC_MODE_MP3                      (*(u32 *)0x11000438)
-#define RES_LEN_EN_PC_MODE_MP3                      (*(u32 *)0x1100043c)
+#define RES_BUF_EN_PAIRING_MP3                      (*(u32 *)0x11000438)
+#define RES_LEN_EN_PAIRING_MP3                      (*(u32 *)0x1100043c)
 
-#define RES_BUF_EN_POWEROFF_MP3                     (*(u32 *)0x11000458)
-#define RES_LEN_EN_POWEROFF_MP3                     (*(u32 *)0x1100045c)
+#define RES_BUF_EN_PC_MODE_MP3                      (*(u32 *)0x11000458)
+#define RES_LEN_EN_PC_MODE_MP3                      (*(u32 *)0x1100045c)
 
-#define RES_BUF_EN_PRIV_CALL_MP3                    (*(u32 *)0x11000478)
-#define RES_LEN_EN_PRIV_CALL_MP3                    (*(u32 *)0x1100047c)
+#define RES_BUF_EN_POWEROFF_MP3                     (*(u32 *)0x11000478)
+#define RES_LEN_EN_POWEROFF_MP3                     (*(u32 *)0x1100047c)
 
-#define RES_BUF_EN_REC_START_MP3                    (*(u32 *)0x11000498)
-#define RES_LEN_EN_REC_START_MP3                    (*(u32 *)0x1100049c)
+#define RES_BUF_EN_PRIV_CALL_MP3                    (*(u32 *)0x11000498)
+#define RES_LEN_EN_PRIV_CALL_MP3                    (*(u32 *)0x1100049c)
 
-#define RES_BUF_EN_RIGHT_CH_MP3                     (*(u32 *)0x110004b8)
-#define RES_LEN_EN_RIGHT_CH_MP3                     (*(u32 *)0x110004bc)
+#define RES_BUF_EN_REC_START_MP3                    (*(u32 *)0x110004b8)
+#define RES_LEN_EN_REC_START_MP3                    (*(u32 *)0x110004bc)
 
-#define RES_BUF_EN_SDCARD_MODE_MP3                  (*(u32 *)0x110004d8)
-#define RES_LEN_EN_SDCARD_MODE_MP3                  (*(u32 *)0x110004dc)
+#define RES_BUF_EN_RIGHT_CH_MP3                     (*(u32 *)0x110004d8)
+#define RES_LEN_EN_RIGHT_CH_MP3                     (*(u32 *)0x110004dc)
 
-#define RES_BUF_EN_SPK_MODE_MP3                     (*(u32 *)0x110004f8)
-#define RES_LEN_EN_SPK_MODE_MP3                     (*(u32 *)0x110004fc)
+#define RES_BUF_EN_SDCARD_MODE_MP3                  (*(u32 *)0x110004f8)
+#define RES_LEN_EN_SDCARD_MODE_MP3                  (*(u32 *)0x110004fc)
 
-#define RES_BUF_EN_TAKE_PHOTO_MP3                   (*(u32 *)0x11000518)
-#define RES_LEN_EN_TAKE_PHOTO_MP3                   (*(u32 *)0x1100051c)
+#define RES_BUF_EN_SPK_MODE_MP3                     (*(u32 *)0x11000518)
+#define RES_LEN_EN_SPK_MODE_MP3                     (*(u32 *)0x1100051c)
 
-#define RES_BUF_EN_TWS_PAIR_MP3                     (*(u32 *)0x11000538)
-#define RES_LEN_EN_TWS_PAIR_MP3                     (*(u32 *)0x1100053c)
+#define RES_BUF_EN_TAKE_PHOTO_MP3                   (*(u32 *)0x11000538)
+#define RES_LEN_EN_TAKE_PHOTO_MP3                   (*(u32 *)0x1100053c)
 
-#define RES_BUF_EN_USB_MODE_MP3                     (*(u32 *)0x11000558)
-#define RES_LEN_EN_USB_MODE_MP3                     (*(u32 *)0x1100055c)
+#define RES_BUF_EN_TWS_PAIR_MP3                     (*(u32 *)0x11000558)
+#define RES_LEN_EN_TWS_PAIR_MP3                     (*(u32 *)0x1100055c)
 
-#define RES_BUF_EN_WAIT4CONN_MP3                    (*(u32 *)0x11000578)
-#define RES_LEN_EN_WAIT4CONN_MP3                    (*(u32 *)0x1100057c)
+#define RES_BUF_EN_USB_MODE_MP3                     (*(u32 *)0x11000578)
+#define RES_LEN_EN_USB_MODE_MP3                     (*(u32 *)0x1100057c)
 
-#define RES_BUF_EQ_CLASSIC_EQ                       (*(u32 *)0x11000598)
-#define RES_LEN_EQ_CLASSIC_EQ                       (*(u32 *)0x1100059c)
+#define RES_BUF_EN_WAIT4CONN_MP3                    (*(u32 *)0x11000598)
+#define RES_LEN_EN_WAIT4CONN_MP3                    (*(u32 *)0x1100059c)
 
-#define RES_BUF_EQ_COUNTRY_EQ                       (*(u32 *)0x110005b8)
-#define RES_LEN_EQ_COUNTRY_EQ                       (*(u32 *)0x110005bc)
+#define RES_BUF_EQ_CLASSIC_EQ                       (*(u32 *)0x110005b8)
+#define RES_LEN_EQ_CLASSIC_EQ                       (*(u32 *)0x110005bc)
 
-#define RES_BUF_EQ_JAZZ_EQ                          (*(u32 *)0x110005d8)
-#define RES_LEN_EQ_JAZZ_EQ                          (*(u32 *)0x110005dc)
+#define RES_BUF_EQ_COUNTRY_EQ                       (*(u32 *)0x110005d8)
+#define RES_LEN_EQ_COUNTRY_EQ                       (*(u32 *)0x110005dc)
 
-#define RES_BUF_EQ_NORMAL_EQ                        (*(u32 *)0x110005f8)
-#define RES_LEN_EQ_NORMAL_EQ                        (*(u32 *)0x110005fc)
+#define RES_BUF_EQ_JAZZ_EQ                          (*(u32 *)0x110005f8)
+#define RES_LEN_EQ_JAZZ_EQ                          (*(u32 *)0x110005fc)
 
-#define RES_BUF_EQ_POP_EQ                           (*(u32 *)0x11000618)
-#define RES_LEN_EQ_POP_EQ                           (*(u32 *)0x1100061c)
+#define RES_BUF_EQ_NORMAL_EQ                        (*(u32 *)0x11000618)
+#define RES_LEN_EQ_NORMAL_EQ                        (*(u32 *)0x1100061c)
 
-#define RES_BUF_EQ_ROCK_EQ                          (*(u32 *)0x11000638)
-#define RES_LEN_EQ_ROCK_EQ                          (*(u32 *)0x1100063c)
+#define RES_BUF_EQ_POP_EQ                           (*(u32 *)0x11000638)
+#define RES_LEN_EQ_POP_EQ                           (*(u32 *)0x1100063c)
+
+#define RES_BUF_EQ_ROCK_EQ                          (*(u32 *)0x11000658)
+#define RES_LEN_EQ_ROCK_EQ                          (*(u32 *)0x1100065c)
 
 #endif
